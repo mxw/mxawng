91672b7115d15e42513d436b9e809b4f3c41fbff
Max Wang <mxawng@gmail.com>
2013-06-28 03:30:50 -0400

    [feature] Adding comment count to index

---
+++ js/disqus-count.js
@@ -0,0 +1,12 @@
+var disqus_shortname = 'mxawng';
+
+(function () {
+  var s = document.createElement('script');
+  s.async = true;
+  s.type = 'text/javascript';
+  s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+
+  var elt = document.getElementsByTagName('HEAD')[0] ||
+            document.getElementsByTagName('BODY')[0];
+  elt.appendChild(s);
+}());
