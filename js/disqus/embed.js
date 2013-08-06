920eddcd99e4689fc2d3b538e423d2f72e08962b
Max Wang <mxawng@gmail.com>
2013-08-06 03:57:31 -0400

    [js/css] Fiddle with comments

--- js/disqus/embed.js
+++ js/disqus/embed.js
@@ -4,7 +4,7 @@ var disqus_shortname = 'mxawng';
   var dsq = document.createElement('script');
   dsq.async = true;
   dsq.type = 'text/javascript';
-  dsq.src = '//' + disqus_shortname + '.disqus.com/count.js';
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 
   var elt = document.getElementsByTagName('head')[0] ||
             document.getElementsByTagName('body')[0];
