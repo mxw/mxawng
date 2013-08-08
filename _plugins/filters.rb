037e7b441f1e5f2728c749a45a4c9e1a55ce6ecf
Max Wang <mxawng@gmail.com>
2013-08-07 21:15:26 -0400

    [jekyll/feature] Add Jekyll filters for meta description tag

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,13 +1,41 @@
 #
 # filters.rb - Utility filters for Liquid.
 #
+# The `truncate`, `condense_spaces`, and `raw_content` filters are adapted from
+# https://github.com/imathis/octopress/blob/master/plugins/octopress_filters.rb.
+#
 
 module Jekyll
-  module MergeFilter
+  module UtilityFilters
     def merge(input, other)
       (input || []) + (other || [])
+    end
+
+    def truncate(input, length)
+      if input.length > length && input[0..(length-1)] =~ /(.+)\b.+$/im
+        $1.strip + ' &hellip;'
+      else
+        input
+      end
+    end
+
+    def truncate_sentences(input, num)
+      input.match(/(?:.*?(\.|!|\?)\s*){1,#{num}}/)[0].strip
+    end
+
+    def condense_spaces(input)
+      input.gsub(/\s{2,}/, ' ')
+    end
+
+    def raw_content(input)
+      /<div class="\w*-content">(?<raw>.*?)<\/div>\s*<(footer|\/article)>/m =~ input
+      return (raw.nil?) ? input : strip_headings(raw)
+    end
+
+    def strip_headings(input)
+      input.gsub(/<h(\d)[^>]*>.*?<\/h\1>/m, '')
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::MergeFilter)
+Liquid::Template.register_filter(Jekyll::UtilityFilters)
